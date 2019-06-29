f7592197732fedddf4e8abf8b562f13d4455cb9d
IvanBabakov <46903548+IvanBabakov@users.noreply.github.com>
2019-06-29 08:05:01 +0300

    Остался чатy

--- websocket/chat/js/chat.js
+++ websocket/chat/js/chat.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const chatConnection = new WebSocket('wss://neto-api.herokuapp.com/chat');
+const chat = document.getElementsByClassName('chat')[0];
+const content = chat.getElementsByClassName('messages-content')[0];
+const messageStatus = chat.querySelector('.message-status');
+const message = chat.querySelector('.messages-templates .message');
+
+chatConnection.addEventListener('open', () => {
+    const btnSendMessage = chat.getElementsByClassName('message-submit')[0];
+    btnSendMessage.removeAttribute('disabled');
+    const chatStatus = chat.querySelector('.chat-status');
+    chatStatus.textContent = chatStatus.getAttribute('data-online');
+    
+    const currentStatus = messageStatus.cloneNode(true);
+    const text = currentStatus.getElementsByClassName('message-text')[0];
+    text.textContent = 'Пользователь появился в сети';
+    content.appendChild(currentStatus);
+})
+chatConnection.addEventListener('message', (event) => {
+    const currentMessage = message.cloneNode(true);
+    const text = currentMessage.getElementsByClassName('message-text')[0];
+    text.textContent = event.data;
+})  
--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const connect = new WebSocket('wss://neto-api.herokuapp.com/counter');
+
+function infoAllConnects(event) {
+    const allOpenAndErrorConnects = JSON.parse(event.data);
+    const allOpenConnects = document.getElementsByClassName('counter')[0];
+    allOpenConnects.textContent = allOpenAndErrorConnects.connections;
+    const allErrorConnects = document.getElementsByClassName('errors')[0];
+    allErrorConnects.textContent = allOpenAndErrorConnects.errors;
+}
+
+connect.addEventListener('message', infoAllConnects);
+window.addEventListener('beforeunload', () => {
+    connect.onclose = function () {};
+    connect.close(1000);
+})
