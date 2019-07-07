4b78da485bd2ae3ab16fa7cfd2a82b120714f2be
IvanBabakov <46903548+IvanBabakov@users.noreply.github.com>
2019-07-07 12:44:20 +0300

    Finish dom-from-zero

--- dom-from-zero/store/js/dom.js
+++ dom-from-zero/store/js/dom.js
@@ -1,1 +1,26 @@
 'use strict';
+
+function createElement(node) {
+
+    function createNode(node) {
+        const nodeName = document.createElement(`${node.name}`); 
+        if(node.props !== null) {
+            if(typeof node.props === 'object') {
+            Object.keys(node.props).forEach(i => nodeName.setAttribute(i, node.props[i]));
+            }
+        }
+        
+        for (let child of node.childs) {
+            if(typeof child == 'string') {
+                const text = document.createTextNode(child);
+                nodeName.appendChild(text);
+            } else {
+                nodeName.appendChild(createNode(child));
+            }
+        }
+        return nodeName;
+    }    
+
+    const newProductDescribe = createNode(node); 
+    return newProductDescribe;
+}
