1223dc2c84907d6bd0b6c90cf45204cecb8a2616
IvanBabakov <46903548+IvanBabakov@users.noreply.github.com>
2019-07-08 09:41:56 +0300

    canvas

--- canvas/background/js/background.js
+++ canvas/background/js/background.js
@@ -1,35 +1,36 @@
 'use strict';
-
-const canvas = document.getElementById('wall');
-const ctx = canvas.getContext('2d');
 const objectQuantity = Math.random()*150 + 50;
 
-canvas.height = window.innerHeight; // масштабирую canvas
-canvas.width = window.innerWidth;
+function draw() {
+  const now = new Date();
+  const canvas = document.getElementById('wall');
+  const ctx = canvas.getContext('2d');
+  canvas.height = window.innerHeight; // масштабирую canvas
+  canvas.width = window.innerWidth;
 
-function nextPoint(x, y, time) {
-    return {
-      x: x + Math.sin((50 + x + (time / 10)) / 100) * 3,
-      y: y + Math.sin((45 + x + (time / 10)) / 100) * 4
-    };
-}
+// function nextPoint(x, y, time) {
+//     return {
+//       x: x + Math.sin((50 + x + (time / 10)) / 100) * 3,
+//       y: y + Math.sin((45 + x + (time / 10)) / 100) * 4
+//     };
+// }
 
-ctx.save();
-for (let i = 0; i <= objectQuantity/2; i++) {
-    const size = Math.random()*(0.6 - 0.1) + 0.1;
-    ctx.strokeStyle = 'white';
-    ctx.lineWidth = 5 * size;
-    ctx.beginPath();
-    const x = Math.random() * canvas.width;
-    const y = Math.random() * canvas.height;
-    const radius = 12 * size;
-    ctx.arc(x, y, radius, 0, 2 * Math.PI);
-    ctx.stroke();
-}
-ctx.restore();
+  ctx.save();
+  for (let i = 0; i <= objectQuantity/2; i++) {
+      const size = Math.random()*(0.6 - 0.1) + 0.1;
+      ctx.strokeStyle = 'white';
+      ctx.lineWidth = 5 * size;
+      ctx.beginPath();
+      const x = Math.random() * canvas.width;
+      const y = Math.random() * canvas.height;
+      const radius = 12 * size;
+      ctx.arc(x, y, radius, 0, 2 * Math.PI);
+      ctx.stroke();
+  }
+  ctx.restore();
 
-ctx.save();
-for (let i = 0; i <= objectQuantity/2; i++) {
+  ctx.save();
+  for (let i = 0; i <= objectQuantity/2; i++) {
     const size = Math.random()*(0.6 - 0.1) + 0.1;
     const angle = (Math.random() * 360) * Math.PI/180;
     const x = Math.random() * canvas.width;
@@ -48,5 +49,8 @@ for (let i = 0; i <= objectQuantity/2; i++) {
     ctx.lineTo(x, y + 20*size);   
     ctx.closePath();
     ctx.stroke();
+  }
+  ctx.restore(); 
 }
-ctx.restore();  
+
+setInterval(draw, 50);
