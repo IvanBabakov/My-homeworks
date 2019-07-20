f3f7cc68af85911dbb45f844e2a918acfc3e9db7
IvanBabakov <46903548+IvanBabakov@users.noreply.github.com>
2019-07-20 20:29:56 +0300

    re-start Drag

--- drag-and-drop/tracking-mouse-cursor/tracking-mouse.js
+++ drag-and-drop/tracking-mouse-cursor/tracking-mouse.js
@@ -5,30 +5,7 @@ const rightEye = document.getElementsByClassName('cat_position_for_rigth_eye')[0
 const cat = document.getElementsByClassName('cat')[0];
 const catEye = document.getElementsByClassName('cat_eye_left')[0];
 
+let minX, minY, maxX, maxY;
 let shiftX = 0;
 let shiftY = 0;
 
-let tic = 0;
-
-document.addEventListener('mousemove', event => {
-    if(tic == 0) {
-        const bonds = event.target.getBoundingClientRect();
-        shiftX = event.pageX - bonds.left - window.pageXOffset;
-        shiftY = event.pageY - bonds.top - window.pageYOffset;    
-        tic = 1;
-    }
-    
-    let x = event.pageX - shiftX;
-    let y = event.pageY - shiftY;
-    // const minX = leftEye.offsetLeft;
-    // const minY = leftEye.offsetTop;
-    // const maxX = leftEye.offsetLeft + leftEye.offsetWidth - catEye.offsetWidth;
-    // const maxY = leftEye.offsetTop + leftEye.offsetHeight - catEye.offsetHeight;
-
-    //  x = Math.min(x, maxX);
-    //  x = Math.max(x, minX);
-
-    catEye.style.left = `${x}px`;  
-    catEye.style.top = `${y}px`;
-
-})
