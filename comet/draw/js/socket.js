908ce9ff277b77b3b2ddbabfed0df843f8fa4b8a
IvanBabakov <46903548+IvanBabakov@users.noreply.github.com>
2019-07-22 11:01:37 +0300

    correction COMET Draw

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -3,10 +3,8 @@
 const ws = new WebSocket('wss://neto-api.herokuapp.com/draw');
 
 editor.addEventListener('update', () => {
-    const ctx = canvas.getContext('2d');
-    const image = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    const binary = Uint8Array.from(image.data);
-    console.log(binary.buffer);
-    ws.send(binary);
+    canvas.toBlob( function(blob) {
+        ws.send(blob);
+    })
 })
 
