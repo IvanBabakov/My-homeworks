639bb86629ce8ee0ce9ee66e24f4615d2e776ca2
IvanBabakov <46903548+IvanBabakov@users.noreply.github.com>
2019-07-23 15:17:18 +0300

    media HW

--- media/photo-booth/js/script.js
+++ media/photo-booth/js/script.js
@@ -1,17 +1,35 @@
 'use strict';
 const source = window.navigator;
 
-if(source !== undefined ) {
     source.mediaDevices.getUserMedia({video: true, audio: false}).then((stream) => {
         const container = document.getElementsByClassName('app')[0];
         const videoElement = document.createElement('video');
         container.appendChild(videoElement);
+        const canvas = document.createElement('canvas');
+        const ctx =  canvas.getContext('2d');
+    
         const video = document.querySelector('video');
         video.srcObject = stream;
         video.onloadedmetadata = function(e) {
             video.play();
         }
-    }).catch(err => console.warn('oh, nooo!'))        
-} else {
-    console.log('error!')
-}
+    
+        const controls = document.getElementsByClassName('controls')[0];
+        controls.style.display = 'inline-block';
+        const button = document.getElementById('take-photo');
+        button.addEventListener('click', () => {          
+            canvas.width = video.clientWidth;
+            canvas.height = video.clientHeight;
+            ctx.drawImage(video, 0, 0);
+            const imageList = document.getElementsByClassName('list')[0];
+            const img = document.createElement('img');
+            const url = canvas.toDataURL();
+            img.src = url;
+            imageList.appendChild(img);
+        })
+    }).catch(err => {
+        const error = document.getElementById('error-message');
+        error.textContent = `${err}`;
+        error.style.display = 'inline-block'; 
+    })       
+
